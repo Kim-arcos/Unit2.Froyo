06c5f6c460ef216095aed7f6f5f60022e0ba629c
Kim-arcos <kimarcos16@gmail.com>
2023-10-02 18:03:09 -0700

    js notes

---
+++ froyo.js
@@ -0,0 +1,9 @@
+//Enter a list of commom separated froyo flavros.
+//vanilla, vanilla, vanilla, strawberry, coffee, coffee
+//When viewing browser console, a table listing of how many of each flavor has been ordered appears
+//Ordered 3 vanilla, 2 coffee, and 1 strawberry
+
+const froyo = {
+    flavors: "vanilla", "strawberry", "coffee"
+}
+console.table(froyo)
