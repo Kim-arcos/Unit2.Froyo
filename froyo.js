7319e5137d4af43f1c37514502f3cae674b67b5b
Kim-arcos <kimarcos16@gmail.com>
2023-10-05 11:51:13 -0700

    more code

--- froyo.js
+++ froyo.js
@@ -2,8 +2,13 @@
 //vanilla, vanilla, vanilla, strawberry, coffee, coffee
 //When viewing browser console, a table listing of how many of each flavor has been ordered appears
 //Ordered 3 vanilla, 2 coffee, and 1 strawberry
-
+let froyo = prompt(
+    "What Froyo flavor would you like: Vanilla, Strawberry, or Coffee?"
+  );
+  let output = createMessage(froyo);
+  console.log(output);
+  
 const froyo = {
-    flavors: "vanilla", "strawberry", "coffee"
+    flavors: "vanilla","strawberry","coffee"
 }
 console.table(froyo)
