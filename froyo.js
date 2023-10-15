f4404677e6f0aea5b0545fed2f715536a48c526f
Kim-arcos <kimarcos16@gmail.com>
2023-10-14 20:53:53 -0700

    finished assignment

--- froyo.js
+++ froyo.js
@@ -2,13 +2,23 @@
 //vanilla, vanilla, vanilla, strawberry, coffee, coffee
 //When viewing browser console, a table listing of how many of each flavor has been ordered appears
 //Ordered 3 vanilla, 2 coffee, and 1 strawberry
-let froyo = prompt(
+  let userInputString = prompt(
     "What Froyo flavor would you like: Vanilla, Strawberry, or Coffee?"
   );
-  let output = createMessage(froyo);
-  console.log(output);
-  
-const froyo = {
-    flavors: "vanilla","strawberry","coffee"
+  console.log(`You ordered: \n${userInputString}`);
+
+  function getFroyoObj (froyoOrder) {
+    const froyoArray = froyoOrder.split(",");
+    const froyoObj = {};
+    for (let i = 0; i < froyoArray.length; i++) {
+      const currentFlavor = froyoArray[i];
+      if (!froyoObj[currentFlavor]) {
+        froyoObj[currentFlavor] = 1;
+    } else {
+      froyoObj[currentFlavor] +=1;
+    }
 }
-console.table(froyo)
+return froyoObj;
+  }
+  console.table(getFroyoObj(userInputString));
+  //I followed along with the class repo. Still trying to see how it works lol
